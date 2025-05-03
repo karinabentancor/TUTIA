56b1ae0be02302c559237c6b646b52393ac6eb85
karinabentancor <karinabentancorlenzi@gmail.com>
2025-05-02 23:28:22 -0300

    welcome

--- server.js
+++ server.js
@@ -1,58 +1,47 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+require('dotenv').config();
 
-// Crear una aplicación Express
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Usar body-parser para leer los datos del formulario
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Configuración del transportador de Nodemailer para Gmail
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'tutiaclub@gmail.com', // Tu correo de Gmail
-    pass: 'tuContraseñaNormal'   // Tu contraseña normal de Gmail
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS
   }
 });
 
-// Ruta para manejar el formulario
 app.post('/register', (req, res) => {
   const { nombre, apellidos, documento, direccion, esquina, celular, email, user, password } = req.body;
 
-  // Configurar el contenido del correo
   const mailOptions = {
-    from: 'tutiaclub@gmail.com', // El correo desde el que se enviará
-    to: 'tutiaclub@gmail.com',   // El correo al que se enviará
+    from: process.env.GMAIL_USER,
+    to: process.env.GMAIL_USER,
     subject: 'Nuevo Registro en el Club',
     text: `
-      Nombre: ${nombre}
-      Apellidos: ${apellidos}
-      Documento: ${documento}
-      Dirección: ${direccion}
-      Esquina: ${esquina}
-      Celular: ${celular}
-      Email: ${email}
-      Usuario: ${user}
-      Contraseña: ${password}
+Nombre: ${nombre}
+Apellidos: ${apellidos}
+Documento: ${documento}
+Dirección: ${direccion}
+Esquina: ${esquina}
+Celular: ${celular}
+Email: ${email}
+Usuario: ${user}
+Contraseña: ${password}
     `
   };
 
-  // Enviar el correo
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      return res.status(500).send('Error al enviar el correo: ' + error);
+      return res.status(500).send('Error al enviar el correo.');
     }
     res.status(200).send('Formulario enviado correctamente!');
   });
 });
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
